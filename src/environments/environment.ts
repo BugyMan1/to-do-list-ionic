f8d563d1a873ec78ef3691c12d980a546cadee66
Yulian Camilo Chavarriaga Restrepo <yucchava@bancolombia.com.co>
2026-01-30 18:04:06 -0500

    refactor: replace sensitive Firebase configuration with placeholders

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC3uhDwaAmvi5BRpxl6IMV34VpFPoBQyxk",
-    authDomain: "todo-app-ionic-b227e.firebaseapp.com",
-    projectId: "todo-app-ionic-b227e",
-    storageBucket: "todo-app-ionic-b227e.firebasestorage.app",
-    messagingSenderId: "301282453810",
-    appId: "1:301282453810:web:bd1e45b17f3ad08d3d0f2b"
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_PROJECT_ID.firebaseapp.com",
+    projectId: "TU_PROJECT_ID",
+    storageBucket: "TU_PROJECT_ID.appspot.com",
+    messagingSenderId: "TU_MESSAGING_SENDER_ID",
+    appId: "TU_APP_ID",
   }
 };
